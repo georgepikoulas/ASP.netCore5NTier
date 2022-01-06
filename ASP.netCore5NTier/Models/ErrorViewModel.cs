30280e03fbc9c4fe7a9644137fcb07b996255ef7
george <geopik@gmail.com>
2022-01-06 19:01:04 +0200

    Add project files.

---
+++ ASP.netCore5NTier/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP.netCore5NTier.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

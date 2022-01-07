e0b0e080ca100d1d281a51abbfd261d8a5c79213
george <geopik@gmail.com>
2022-01-07 12:57:30 +0200

    Added Lsist for Categories view

--- ASP.netCore5NTier/Controllers/CategoryController.cs
+++ ASP.netCore5NTier/Controllers/CategoryController.cs
@@ -3,14 +3,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASP.netCore5NTier.Data;
+using ASP.netCore5NTier.Models;
 
 namespace ASP.netCore5NTier.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDBContext _db;
+
+
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> categories = _db.Category;
+            return View(categories);
         }
     }
 }

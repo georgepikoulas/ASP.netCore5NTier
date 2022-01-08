02f0fb5a89b4d0ab97e813433b85b1c4d25b2940
george <geopik@gmail.com>
2022-01-08 20:52:33 +0200

    Added Edit fuinctionality for Category EF 5 has and Update function that requires passing though the Primary key of the Entity . so we pass ot as a hidden input

--- ASP.netCore5NTier/Controllers/CategoryController.cs
+++ ASP.netCore5NTier/Controllers/CategoryController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ASP.netCore5NTier.Data;
 using ASP.netCore5NTier.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ASP.netCore5NTier.Controllers
 {
@@ -38,6 +39,37 @@ namespace ASP.netCore5NTier.Controllers
             if (ModelState.IsValid)
             {
                 _db.Category.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        //Get Edit 
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Category.FirstOrDefault(p => p.Id == id);
+
+            if (obj == null)
+                return NotFound();
+
+            return View(obj);
+        }
+
+        //POST create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update( category);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }

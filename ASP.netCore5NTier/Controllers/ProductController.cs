9a887d6a4142635e65a5ecb3136a6748783c5bbc
george <geopik@gmail.com>
2022-01-09 22:21:22 +0200

    Added Prodct ViewModel to keep the view strongly typed

--- ASP.netCore5NTier/Controllers/ProductController.cs
+++ ASP.netCore5NTier/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ASP.netCore5NTier.Data;
 using ASP.netCore5NTier.Models;
+using ASP.netCore5NTier.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
@@ -33,31 +34,42 @@ namespace ASP.netCore5NTier.Controllers
         //Get Upsert 
         public IActionResult Upsert(int? id)
         {
-            IEnumerable<SelectListItem> CategoryDropDown = _db.Category.Select(p => new SelectListItem
+            //IEnumerable<SelectListItem> CategoryDropDown = _db.Category.Select(p => new SelectListItem
+            //{
+
+            //    Text = p.Name,
+            //    Value = p.Id.ToString()
+            //});
+
+            //ViewBag.CategoryDropDown = CategoryDropDown;
+
+            //var product = new Product();
+            ProductVM productVM = new ProductVM()
             {
+                Product = new Product(),
+                CategoryListItems = _db.Category.Select(p => new SelectListItem
+                {
 
-                Text = p.Name,
-                Value = p.Id.ToString()
-            });
+                    Text = p.Name,
+                    Value = p.Id.ToString()
+                })
+            };
 
-            ViewBag.CategoryDropDown = CategoryDropDown;
-
-            var product = new Product();
             if (id == null)
             {
                 //this is for create
-                return View(product);
+                return View(productVM);
             }
             else
             {
-                product = _db.Product.FirstOrDefault(p => p.Id == product.Id);
-                if (product == null)
+                productVM.Product = _db.Product.FirstOrDefault(p => p.Id == productVM.Product.Id);
+                if (productVM == null)
                 {
                     return NotFound();
                 }
                 else
                 {
-                    return View(product);
+                    return View(productVM);
                 }
             }
 
@@ -78,66 +90,6 @@ namespace ASP.netCore5NTier.Controllers
             return View();
         }
 
-        //Get Edit 
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
 
-            var obj = _db.Category.FirstOrDefault(p => p.Id == id);
-
-            if (obj == null)
-                return NotFound();
-
-            return View(obj);
-        }
-
-        //POST edit
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(Category category)
-        {
-            if (ModelState.IsValid)
-            {
-                _db.Category.Update(category);
-                _db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View();
-        }
-
-        //Get Delete 
-        public IActionResult Delete(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-
-            var obj = _db.Category.Find(id);
-
-            if (obj == null)
-                return NotFound();
-
-            return View(obj);
-        }
-
-        //POST edit
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeletePost(int? id)
-        {
-            var obj = _db.Category.Find(id);
-            if (obj == null)
-                return NotFound();
-
-            _db.Category.Remove(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
-
-        }
     }
 }

9dc9603fb976bc8a331c96979a6aeadce329e5dc
george <geopik@gmail.com>
2022-01-11 14:27:45 +0200

    Added scaffoldiong UI Added nav item woth the LoginPartiial

--- ASP.netCore5NTier/Startup.cs
+++ ASP.netCore5NTier/Startup.cs
@@ -60,12 +60,12 @@ namespace ASP.netCore5NTier
             app.UseStaticFiles();
 
             app.UseRouting();
-
             app.UseAuthentication();
             app.UseAuthorization();
             app.UseSession(); 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");

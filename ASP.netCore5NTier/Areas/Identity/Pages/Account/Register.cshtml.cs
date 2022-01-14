17c8ce527c6b1339822d4f1bfa03349992fbb040
george <geopik@gmail.com>
2022-01-14 12:21:10 +0200

    Added fix becaue the Create Admin USer when a suer ws logged would auto Sign In with new user , we did not want that

--- ASP.netCore5NTier/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ASP.netCore5NTier/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -122,7 +122,15 @@ namespace ASP.netCore5NTier.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if (!User.IsInRole(WC.AdminRole))
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+    
+                        }
+                        else
+                        {
+                            return RedirectToAction(nameof(Index));
+                        }
                         return LocalRedirect(returnUrl);
                     }
                 }

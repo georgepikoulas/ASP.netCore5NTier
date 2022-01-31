328a47ed85dd163c9dd2c2a599a1d81a8d59eccf
george <geopik@gmail.com>
2022-01-31 12:56:54 +0200

    AddedApplicationUSer repo

---
+++ ASP.netCore5NTier.Data/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ASP.netCore5NTier.Data.Repository.IRepository;
+using ASP.netCore5NTier.Models;
+
+namespace ASP.netCore5NTier.Data.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private readonly ApplicationDBContext  _db;
+        public ApplicationUserRepository(ApplicationDBContext db) : base(db)
+        {
+            _db = db;
+        }
+
+    }
+}

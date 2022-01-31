e14b1ef8b23ceb905d2c35c0010e507a62afe401
george <geopik@gmail.com>
2022-01-31 12:28:31 +0200

    Added Repositories

---
+++ ASP.netCore5NTier.Data/Repository/IRepository/InquiryHeaderRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ASP.netCore5NTier.Models;
+
+namespace ASP.netCore5NTier.Data.Repository.IRepository
+{
+    public class InquiryHeaderRepository : Repository<InquiryHeader>, IInquiryHeaderRepository
+    {
+        private readonly ApplicationDBContext _db;
+        public InquiryHeaderRepository(ApplicationDBContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(InquiryHeader obj)
+        {
+            _db.InquiryHeader.Update(obj);
+        }
+    }
+}
---
+++ ASP.netCore5NTier.Data/Repository/InquiryDetailRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ASP.netCore5NTier.Data.Repository.IRepository;
+using ASP.netCore5NTier.Models;
+
+namespace ASP.netCore5NTier.Data.Repository
+{
+    public class InquiryDetailRepository : Repository<InquiryDetail>, IInquiryDetailRepository
+    {
+        private readonly ApplicationDBContext _db;
+        public InquiryDetailRepository(ApplicationDBContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(InquiryDetail obj)
+        {
+            _db.InquiryDetail.Update(obj);
+        }
+    }
+}

37a13f70b06b5f559cce020d2df0fadc9a498f11
george <geopik@gmail.com>
2022-02-02 17:10:35 +0200

    Added InquiryController

---
+++ ASP.netCore5NTier/Controllers/InquiryController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP.netCore5NTier.Data.Repository.IRepository;
+using ASP.netCore5NTier.Models;
+using ASP.netCore5NTier.Utility;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ASP.netCore5NTier.Controllers
+{
+    [Authorize(Roles = WC.AdminRole)]
+    public class InquiryController : Controller
+    {
+        private readonly IInquiryHeaderRepository _inquiryHeaderRepository;
+        private readonly IInquiryDetailRepository _inquiryDetailRepository;
+
+        public InquiryController(IInquiryHeaderRepository inquiryHeaderRepository, IInquiryDetailRepository inquiryDetailRepository)
+        {
+            _inquiryHeaderRepository = inquiryHeaderRepository;
+            _inquiryDetailRepository = inquiryDetailRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
